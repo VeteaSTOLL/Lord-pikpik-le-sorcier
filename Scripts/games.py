ac54de24b7aed877e73f7ae983320b9ebe2800c0
Bulbiii <167900389+Bulbiii@users.noreply.github.com>
2024-10-07 17:40:48 -0400

    Ajout des plateformes et gestions de leur collisions

--- Scripts/games.py
+++ Scripts/games.py
@@ -1,9 +1,13 @@
 import pygame
 from joueur import joueur
+from joueur import roulette
+from platformClass import Platform
+
 
 class Game:
     def __init__(self):
         #generer notre joueur
+        self.roulette = roulette()
         self.joueur = joueur()
         
         self.pressed = {}
@@ -11,3 +15,41 @@ class Game:
         self.pressed_up = {}
 
         self.crafting_interface = False
+        
+        #////////////////PLATEFRORMS//////////
+        
+        # Liste des rectangles de plateformes
+        self.platform_lis_rect = [pygame.Rect(30, 600, 300, 50), pygame.Rect(400, 400, 300, 50)] #(x,y (point supp gauche),largeur,hauteur)
+
+        # Liste des plateformes
+        self.platforms = []
+        
+        # Créer des objets Platform à partir de la liste des rectangles
+        for rect in self.platform_lis_rect:
+            platform = Platform(rect)
+            self.platforms.append(platform)  # Ajouter les plateformes à la liste
+        
+    def display_platforms(self, surface):
+        # Parcourir et afficher chaque plateforme dans la liste
+        for platform in self.platforms:
+            platform.display(surface)
+
+
+
+    def check_collisions(self):
+        #Vérifie les collisions du joueur avec les plateformes
+        player_rect = self.joueur.rect
+
+        # Parcourir les plateformes
+        for platform in self.platforms:
+            if player_rect.colliderect(platform.rect):
+                # Si le joueur est en train de descendre et touche une plateforme
+                if self.joueur.velocity_y > 0 and player_rect.bottom <= platform.rect.top + 10:
+                    self.joueur.rect.bottom = platform.rect.top  # Positionner le joueur sur le dessus de la plateforme
+                    self.joueur.pos_y = self.joueur.rect.y  # Corriger la position en y
+                    self.joueur.velocity_y = 0  # Arrêter la chute
+                    self.joueur.is_jumping = False  # Il n'est plus en saut
+                    break
+        else:
+            # Si aucune collision, le joueur est en l'air
+            self.joueur.is_jumping = True
