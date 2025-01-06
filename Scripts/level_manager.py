a41e45e325cdaba4e1bb8816f88b2919faaf4490
Bulbiii <fauconchloe2@gmail.com>
2025-01-06 23:06:23 +0100

    ajout de niveaux + texte introduction regles

--- Scripts/level_manager.py
+++ Scripts/level_manager.py
@@ -15,7 +15,7 @@ class Level_manager:
         self.video = "video/End.mp4"
         
         
-        self.nb_level = 3
+        self.nb_level = 5
         self.levels=[
             {
                 "collisions" : [                
@@ -35,31 +35,88 @@ class Level_manager:
                     (self.item_manager.botte, 4, 13, 1, 1),                  
                 ]         
             },
-            {
-                "collisions" :[
-                    (0, -1, 20, 1),#Plafond
-                    (20, 0, 1, 16), #bord écran droit
-                    (-1, 0, 1, 16),#Bord écran gauche
-                    (0, 14, 20, 4),
-                    (18, 4, 2, 1),
-                    (18, 6, 2, 1),
-                    (13, 5, 1, 5),
-                    (3, 7, 1, 1),
-                    (10, 6, 1, 1),
-                    (6, 3, 1, 5),
-                    (9, 12, 1, 1),
-
-                    
-                    
+                         {
+                "collisions": [
+                    (0, -1, 20, 1),  #Plafond
+                    (20, 0, 3, 16),  #bord écran droit
+                    (-1, 0, 1, 16),  #Bord écran gauche
+                    (0, 14, 23, 4),  #sol
+                    (6, 12, 3, 1),  
+                    (12, 10, 4, 1),  
+                    (3, 8, 3, 1),    
+                    (8, 6, 2, 1),    
+                    (15, 4, 2, 1),   
+                    (2, 4, 2, 1),    
+                    (10, 13, 1, 1), 
+                    (16, 13, 1, 1), 
+                    (5, 9, 1, 1),    
                 ],
-                
-                "items":[
-                    (self.item_manager.aile, 3, 13, 1, 1),
-                    (self.item_manager.porte, 19,5,1,1),
+
+                "items": [
+                    (self.item_manager.porte, 16, 3, 1, 1),    
+                    (self.item_manager.botte, 7, 11, 1, 1),     
+                    (self.item_manager.roulette, 5, 7, 1, 1),
                 ]
             },
             {
                 "collisions" :[
+                    (0, -1, 20, 1),#Plafond
+                    (20, 0, 3, 16), #bord écran droit
+                    (-1, 0, 1, 16),#Bord écran gauche
+                    (0, 14, 23, 4), #sol
+                    (0,1,1,1),
+                    (18,12,1,1),
+                    (17,11,1,1),
+                    (16,10,1,1),
+                    (19,9,1,2),
+                    (18,8,1,1),
+                    (16,8,1,2),
+                    (13,8,4,1),
+                    (6,8,1,1),
+                    (19,5,1,4),
+                    (2,4,1,5),
+                    (3,8,1,1),
+                    (15,2,3,1),
+                    (11,4,3,1),
+                    (9,3,1,1),
+                    (4,4,1,1),
+                    (7,2,1,1),
+                ],
+                
+                "items":[
+                    (self.item_manager.porte, 0, 0, 1, 1),
+                    (self.item_manager.botte, 18, 13, 1, 1),
+                    (self.item_manager.roulette, 16, 7, 1, 1)
+
+                ]
+            },
+            {
+                "collisions" :[
+                    (0, -1, 20, 1),#Plafond
+                    (23, 0, 3, 16), #bord écran droit
+                    (-1, 0, 1, 16),#Bord écran gauche
+                    (1, 15, 2, 4), #sol
+                    (5, 15, 2, 4),
+                    (9, 15, 2, 4),
+                    (14, 15, 2, 4),
+                    (18, 11, 5, 1),
+                    (21, 7, 2, 1),
+                    (17, 3, 2, 1),
+                    (12, 3, 2, 1),
+                    (6, 7, 2, 1),
+                    (3, 5, 1, 1),   
+                ],
+                
+                "items":[
+                    (self.item_manager.botte, 1, 14, 1, 1),
+                    (self.item_manager.roulette, 2, 14, 1, 1),
+                    (self.item_manager.porte, 3, 4, 1, 1)
+                    ],           
+            },
+            {
+                "collisions" :[
+                    (20, 0, 3, 16),  #bord écran droit
+                    (-1, 0, 1, 16),  #Bord écran gauche
                     (0, 14, 20, 4),
                     (2, 5, 1, 5),
                     (3, 5, 2, 1),
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,5 @@
 import pygame
 from games import Game
-#from moviepy.editor import VideoFileClip
 
 
 pygame.init()
@@ -12,6 +11,8 @@ pygame.display.set_caption("Bouboule")
 screen = pygame.display.set_mode((1000, 750)) 
 
 running = True 
+affiche_regle = True
+start_game = False
 
 #charger notre jeu
 game = Game(screen)
@@ -20,6 +21,9 @@ niveau = game.level_manager.creer_niveau(current_level)
 
 game.collision_list = niveau.get_collisions()
 game.item_manager.objects = niveau.get_objects()
+
+font_title = pygame.font.Font(None, 48)
+font_text = pygame.font.Font(None, 36)
 
 while running:
     #permet de renseigner la variable "dt" qui est le différentiel de temps entre l'avant dernière frame et la dernière frame, prartique pour pouvoir rendre le gameplay indépendant du framerate
@@ -40,6 +44,9 @@ while running:
         if event.type == pygame.KEYDOWN:
             game.pressed[event.key] = True
             game.pressed_down[event.key] = True
+            if affiche_regle and event.key ==   pygame.K_s: 
+                    affiche_regle = False
+                    start_game = True
         if event.type == pygame.KEYUP:
             game.pressed[event.key] = False
             game.pressed_up[event.key] = True
@@ -51,11 +58,42 @@ while running:
             game.pressed["clic_souris"] = False
             game.pressed_up["clic_souris"] = True
     
+    if affiche_regle:
+        screen.fill((135, 206, 250))
+
+        regles = [
+            " Lord PIKPIK le sorcier !",
+            " ",
+            "Résolvez tous les niveaux afin de finir le jeu.",
+            "",
+            "Touches :",
+            "           * Vous déplacer avec les flèches et sauter avec la barre espace",
+            "           * Appuyer sur E pour acceder à l'espace de craft",
+            "           * Appuyer sur R pour recommencer le niveau",
+            "",
+            "",
+            " Il est l'heure de commencer, appuyez sur S",
+            "Amusez-vous !"
+        ]
+        titre = font_title.render(regles[0], True, (255,255,255))
+        titre_rect = titre.get_rect(center=(1000 // 2, 100)) 
+        screen.blit(titre, titre_rect)
+
+        
+        y_offset = 160  
+        for ligne in regles[1:]:
+            texte = font_text.render(ligne, True, (128, 128, 128))
+            texte_rect = texte.get_rect(left=50, top=y_offset) 
+            screen.blit(texte, texte_rect)
+            y_offset += 40
+   
+    
     if current_level > game.level_manager.nb_level:
         running = False
     else:
         game.collision_list = niveau.get_collisions()
         game.item_manager.objects = niveau.get_objects()
+    pygame.display.flip()
 
 
     if not game.interface_craft.is_open:
@@ -110,39 +148,35 @@ while running:
         game.joueur.reset_body()
         game.interface_craft.reset_interface()
     
-    # if current_level == game.level_manager.nb_level:
-    #     clip = VideoFileClip('video/end.mp4')
-    #     clip.preview()
-    
-    
-    #affichage
-    screen.fill((255,255,255))
-    
-    game.item_manager.draw_objects(screen)
-
-    game.joueur.draw(screen, game.item_manager.item_types)
-
-    game.collision_list.draw_collisions(screen)    
-
-    if game.debug.debug_mode:
-        game.debug.draw(screen, game.joueur.body, game.joueur.destination, fps, game.joueur.direction)
-
-    
-    if game.interface_craft.is_open:
-        #on affiche l'UI pour le craft du perso
-        game.interface_craft.draw_crafting_interface(screen)
-    if game.interface_item_discovery.is_open:
-        game.interface_item_discovery.draw(screen)
-
-    if  game.joueur.pos[1]>750 :
-        current_level = 1
-        image = pygame.image.load("img/game_over.png")
-        game.level_manager.game_over_sound.play()
-        screen.fill((0, 0, 0))
-        screen.blit(image,(0,0))
+    if not affiche_regle :
+        #affichage
+        screen.fill((255,255,255))
         
-    
-    pygame.display.flip()
-    
+        game.item_manager.draw_objects(screen)
+
+        game.joueur.draw(screen, game.item_manager.item_types)
+
+        game.collision_list.draw_collisions(screen)    
+
+        if game.debug.debug_mode:
+            game.debug.draw(screen, game.joueur.body, game.joueur.destination, fps, game.joueur.direction)
+
+        
+        if game.interface_craft.is_open:
+            #on affiche l'UI pour le craft du perso
+            game.interface_craft.draw_crafting_interface(screen)
+        if game.interface_item_discovery.is_open:
+            game.interface_item_discovery.draw(screen)
+
+        if  game.joueur.pos[1]>750 :
+            #current_level = 1
+            image = pygame.image.load("img/game_over.png")
+            game.level_manager.game_over_sound.play()
+            screen.fill((0, 0, 0))
+            screen.blit(image,(0,0))
+            
+        
+        pygame.display.flip()
         
         
+      
